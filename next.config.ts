9bc1e27e3b7a9a0505eb9b93f4473c08f82d1a3a
Yee Chern Goh <yeecherngoh@Yees-MacBook-Pro.local>
2026-01-29 16:50:56 +0800

    test1

--- next.config.ts
+++ next.config.ts
@@ -9,4 +9,7 @@ export default withPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
+  workboxOptions: {
+    importScripts: ["/push-sw.js"],
+  },
 })(nextConfig);
